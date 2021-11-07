f528b78c9a948ddc4da8351ec81052c5526481b5
TheBrothers05 <89668073+TheBrothers05@users.noreply.github.com>
2021-11-07 02:41:29 -0600

    MODIFICACION

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@ var url = window.location.href;
 var swLocation = '/twittor/sw.js';
 
 if(navigator.serviceWorker){
-    if(url.includes('localhost'))
+    if(url.includes('localhost')){
     swLocation='/sw.js';
+    }
 
 
     navigator.serviceWorker.register(swLocation);
